92b9d2dced05c4663d0e589fd3678c51b2d7b301
ronald360@gmail.com <ronald360@gmail.com>
2021-11-22 13:42:49 +0200

    Completed module

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,56 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.UUID;
 
+@Entity(name="users")
 public class User implements Serializable {
+
+    @Id
+    UUID id;
+
+    @Column
+    String username;
+
+    @Column
+    String password;
+
+    @Column
+    boolean enabled = true;
+
+    User(){}
+
+    public User(String username, String password) {
+        this.id = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public void setId(UUID id){
+        this.id=id;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public boolean getEnabled(){
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled){
+        this.enabled=enabled;
+    }
+
 }
